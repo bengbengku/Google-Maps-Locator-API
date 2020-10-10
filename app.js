247357ffb0dc99962509e0c958385ffe052e34ae
bengbengku <anggiatbenget70@gmail.com>
2020-10-10 21:53:04 +0700

    ADD: Testing store data ke mongo db

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
+const Store = require('./api/models/store');
 
 mongoose.connect("mongodb+srv://bengbengku:1Ov3WY4fqOWoymum@cluster0.ulte2.mongodb.net/<dbname>?retryWrites=true&w=majority", { 
   useNewUrlParser: true,
@@ -13,6 +14,18 @@ app.use(express.json({ limit: "50mb" }));
 app.post("/api/stores", (req, res) => {
   let dbStores = req.body;
   console.log(dbStores);
+  const store = new Store({
+    storeName: "Test",
+    phoneNumber: "089283839",
+    location: {
+      type: 'Point',
+      coordinates: [
+        98.627757,
+        3.591192
+      ]
+    }
+  })
+  store.save();
   res.send("You have posted");
 });
 

53a878e90f7f65f3d45b07163f2035eb59ca0b0e
bengbengku <anggiatbenget70@gmail.com>
2020-10-10 20:50:30 +0700

    ADD: testing post data dengan POSTMAN

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const express = require('express');
 const app = express()
 const port = 3000
 
+
+app.use(express.json({ limit: '50mb' }));
+
+app.post('/api/stores', (req, res) => {
+  let dbStores = req.body;
+  console.log(dbStores);
+  res.send("You have posted");
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
-})
+});

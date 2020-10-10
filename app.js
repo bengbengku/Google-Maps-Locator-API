90cd5573dd6cb1a5acdcb9b42f6418191e773e3c
bengbengku <anggiatbenget70@gmail.com>
2020-10-10 22:00:10 +0700

    ADD: Testing delete stores

--- app.js
+++ app.js
@@ -14,24 +14,19 @@ app.use(express.json({ limit: "50mb" }));
 app.post("/api/stores", (req, res) => {
   let dbStores = req.body;
   console.log(dbStores);
-  const store = new Store({
-    storeName: "Test",
-    phoneNumber: "089283839",
-    location: {
-      type: 'Point',
-      coordinates: [
-        98.627757,
-        3.591192
-      ]
-    }
-  })
-  store.save();
+ 
   res.send("You have posted");
 });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.delete('/api/stores', (req, res) => {
+  Store.deleteMany({}, (err) => {
+    res.status(200).send(err);
+  })
+})
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);

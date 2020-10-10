cbf2b061ef1e66e5b7878dd149daab200c1710f2
bengbengku <anggiatbenget70@gmail.com>
2020-10-10 21:19:02 +0700

    ADD: Koneksi mongoose ke mongo DB

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-const express = require('express');
-const app = express()
-const port = 3000
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const port = 3000;
 
+mongoose.connect("mongodb+srv://bengbengku:1Ov3WY4fqOWoymum@cluster0.ulte2.mongodb.net/<dbname>?retryWrites=true&w=majority", { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
+});
 
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
 
-app.post('/api/stores', (req, res) => {
+app.post("/api/stores", (req, res) => {
   let dbStores = req.body;
   console.log(dbStores);
   res.send("You have posted");
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`);
 });
